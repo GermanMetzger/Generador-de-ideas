a34f3ba0397fc8b17e93b68b61e71dd5fd80e7ff
GermanMetzger <metzgergerman@gmail.com>
2025-08-11 23:57:36 -0300

    boton copiar

--- src/components/Transcripciones/Transcripcion.js
+++ src/components/Transcripciones/Transcripcion.js
@@ -3,6 +3,16 @@ import React from 'react'
 
 import "./Transcripcion.css"
 
+const copiarAlPortapapeles = (texto) => {
+  navigator.clipboard.writeText(texto)
+    .then(() => {
+      alert("Texto copiado al portapapeles");
+    })
+    .catch((error) => {
+      console.error("Error al copiar al portapapeles:", error);
+    });
+};
+
 
 export default function Transcripcion({ urlYResultado, eliminar, index }) {
   return (
@@ -10,7 +20,8 @@ export default function Transcripcion({ urlYResultado, eliminar, index }) {
       <div>
         <h3>URL: {urlYResultado.url}</h3>
         <p>Transcripción: {urlYResultado.resultado}</p>
-        <button onClick={() => eliminar(index)}>Eliminar</button>
+        <button onClick={() => {copiarAlPortapapeles(urlYResultado.resultado); eliminar(index);}}>Copiar y Eliminar</button>
+        <button style={{ backgroundColor: 'red', color: 'white', margin: '5px' }} onClick={() => eliminar(index)}>Eliminar</button>
       </div>
     </div>
   )
